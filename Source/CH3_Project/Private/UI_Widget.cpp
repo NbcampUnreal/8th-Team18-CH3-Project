d9c6f17e424e958b21048ab58f976e9d51c5efc3
poe444 <ena10273@gmail.com>
2026-05-13 14:40:28 +0900

    [05-13]UI 관련커밋

--- Source/CH3_Project/Private/UI_Widget.cpp
+++ Source/CH3_Project/Private/UI_Widget.cpp
@@ -1,4 +1,44 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+//UI_Widget.cpp
 
 
 #include "UI_Widget.h"
+#include "MainCharacter.h"
+#include "Kismet/GameplayStatics.h"
+#include "Components/ProgressBar.h"
+#include "Components/TextBlock.h"
+
+
+void UUI_Widget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	MyCharacter = Cast<AMainCharacter>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
+	if (MyCharacter)
+	{
+		MyCharacter->HealthDamaged.AddUniqueDynamic(this, &UUI_Widget::UpdateHP);
+		UpdateHP(MyCharacter->GetHealth(), 100.f, 100.0f);
+	}
+	
+
+}
+
+void UUI_Widget::UpdateHP(float UpdatePlayerHP, float PlayerMaxHP, float CurrentPlayerHP)
+{
+	if (HP_Bar)
+	{
+		if (PlayerMaxHP > 0.0f)
+		{
+			float UpdatePlayerHP = CurrentPlayerHP / PlayerMaxHP;
+			HP_Bar->SetPercent(UpdatePlayerHP);
+		}
+		
+	}
+}
+
+void UUI_Widget::UpdateAmmo(int32 CurrentAmmo, int32 MaxAmmo)
+{
+	if (Ammo_Text)
+	{
+		FString AmmoString = FString::Printf(TEXT("%d / %d"), CurrentAmmo, MaxAmmo);
+		Ammo_Text->SetText(FText::FromString(AmmoString));
+	}
+}
--- Source/CH3_Project/Public/UI_Widget.h
+++ Source/CH3_Project/Public/UI_Widget.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+//UI_Widget.h
 
 #pragma once
 
@@ -6,16 +6,31 @@
 #include "Blueprint/UserWidget.h"
 #include "UI_Widget.generated.h"
 
-/**
- * 
- */
+
+class UProgressBar;
+class UTextBlock;
+class UImage;
+
 UCLASS()
 class CH3_PROJECT_API UUI_Widget : public UUserWidget
 {
 	GENERATED_BODY()
-	
+public:
+	class AMainCharacter* MyCharacter;
 
 protected:
+		UPROPERTY(meta = (BindWidget))
+		UProgressBar* HP_Bar;
+		UPROPERTY(meta = (BindWidget))
+		UTextBlock* Ammo_Text;
+		UPROPERTY(meta = (BindWidget))
+		UImage* Crosshair;
+		UPROPERTY(meta = (BindWidget))
+		UTextBlock* Score_Text;
 
-
+public:
+	UFUNCTION()
+	void UpdateHP(float UpdatePlayerHP, float PlayerMaxHP, float CurrentPlayerHP);
+	virtual void NativeConstruct()override;
+	void UpdateAmmo(int32 CurrentAmmo, int32 MaxAmmo);
 };
